b01792eb6dcdd754868a6f16e42357731384ccb1
Lucas Leonel Lopez <tre.lopez@hotmail.com>
2021-07-21 23:36:01 -0300

    apiusers post

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('./models/user')
 
 router.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
@@ -7,6 +8,18 @@ router.get('/', (req, res) => {
 
 router.get('/api/user', (req, res, next) => {
     res.json({ buenas: 'hola' })
+})
+
+router.post('/api/users', async (req, res, next) => {
+    const { username } = req.body;
+    const newUser = new User();
+    newUser.username = username;
+    const savedUser = await newUser.save();
+    const jsonRes = {
+        username: savedUser.username,
+        id: savedUser.id
+    }
+    return res.json(jsonRes)
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('./database');
 
 //MIDDLEWARES
 app.use(cors());
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 //cada vez que se pida una ruta usa el router propio exportado desde routes.js
 app.use('/', require('./routes'))
